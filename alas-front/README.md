da0286e79796550104b5f7d2e64266fae2d3d685
sporlichka <tvooybatya@gmail.com>
2025-04-23 15:00:09 +0500

    aa

--- alas-front/README.md
+++ alas-front/README.md
@@ -1,4 +1,4 @@
-# AlasApp
+# AlasFront
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.1.7.
 
--- alas-front/src/app/app.component.spec.ts
+++ alas-front/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'alas-app' title`, () => {
+  it(`should have the 'alas-front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('alas-app');
+    expect(app.title).toEqual('alas-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, alas-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, alas-front');
   });
 });
